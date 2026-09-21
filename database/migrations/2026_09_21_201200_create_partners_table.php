fe75a022749285e15ab0a04bc069685d69a7317d
Proxy for Main <149622634+deputy-proxy@users.noreply.github.com>
2026-09-21 20:33:36 +0300

    #15 lint fix

--- database/migrations/2026_09_21_201200_create_partners_table.php
+++ database/migrations/2026_09_21_201200_create_partners_table.php
@@ -6,7 +6,9 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /** Run the migrations. */
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
         Schema::create('partners', function (Blueprint $table) {
@@ -22,7 +24,9 @@ return new class extends Migration
         });
     }
 
-    /** Reverse the migrations. */
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
         Schema::dropIfExists('partners');
