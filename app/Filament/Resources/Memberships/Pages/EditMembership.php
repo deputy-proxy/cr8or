ed74e667b437f1cc96abc14fba87c48ae22eb25c
Proxy for Main <149622634+deputy-proxy@users.noreply.github.com>
2026-09-21 21:37:28 +0300

    Fix #17 Membership reassignment guard

--- app/Filament/Resources/Memberships/Pages/EditMembership.php
+++ app/Filament/Resources/Memberships/Pages/EditMembership.php
@@ -8,8 +8,9 @@ use Illuminate\Auth\Access\AuthorizationException;
 class EditMembership extends EditRecord {
  protected static string $resource=MembershipResource::class;
  protected function mutateFormDataBeforeSave(array $data):array{
-  /** @var Membership $record */ $record=$this->record;
-  if((int)$data['${parent}_id']!==$record->${parent}_id){throw new AuthorizationException('Cannot reassign this record.');}
+  /** @var Membership $record */
+  $record=$this->record;
+  if((int)$data['organization_id']!==$record->organization_id){throw new AuthorizationException('Cannot reassign this record.');}
   return $data;
  }
  protected function getHeaderActions():array{return [DeleteAction::make()];}
