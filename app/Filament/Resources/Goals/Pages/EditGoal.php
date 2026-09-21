e9603b6fa882e169f26393573ec448fc63543e1e
Proxy for Main <149622634+deputy-proxy@users.noreply.github.com>
2026-09-21 21:37:44 +0300

    Fix #17 Goal reassignment guard

--- app/Filament/Resources/Goals/Pages/EditGoal.php
+++ app/Filament/Resources/Goals/Pages/EditGoal.php
@@ -8,8 +8,9 @@ use Illuminate\Auth\Access\AuthorizationException;
 class EditGoal extends EditRecord {
  protected static string $resource=GoalResource::class;
  protected function mutateFormDataBeforeSave(array $data):array{
-  /** @var Goal $record */ $record=$this->record;
-  if((int)$data['${parent}_id']!==$record->${parent}_id){throw new AuthorizationException('Cannot reassign this record.');}
+  /** @var Goal $record */
+  $record=$this->record;
+  if((int)$data['enterprise_id']!==$record->enterprise_id){throw new AuthorizationException('Cannot reassign this record.');}
   return $data;
  }
  protected function getHeaderActions():array{return [DeleteAction::make()];}
