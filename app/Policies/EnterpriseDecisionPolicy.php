3913b8d9821e934192e96c05ccb1af4f5e1ac193
Proxy for Main <149622634+deputy-proxy@users.noreply.github.com>
2026-09-21 20:53:02 +0300

    #16 add EnterpriseDecisionPolicy.php

---
+++ app/Policies/EnterpriseDecisionPolicy.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Enterprise;
+use App\Models\EnterpriseDecision;
+use App\Models\User;
+
+class EnterpriseDecisionPolicy
+{
+    public function view(User $user, EnterpriseDecision $decision): bool
+    {
+        return $this->enterprisePolicy()->view($user, $decision->enterprise);
+    }
+
+    public function create(User $user, Enterprise $enterprise): bool
+    {
+        return $this->enterprisePolicy()->create($user, $enterprise->organization);
+    }
+
+    public function update(User $user, EnterpriseDecision $decision): bool
+    {
+        return $this->enterprisePolicy()->update($user, $decision->enterprise);
+    }
+
+    private function enterprisePolicy(): EnterprisePolicy
+    {
+        return new EnterprisePolicy;
+    }
+}
