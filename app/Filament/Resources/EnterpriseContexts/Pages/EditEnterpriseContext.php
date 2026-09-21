ad230ffe7312cd0cd292d8ae37456fcfbe3a8cef
Proxy for Main <149622634+deputy-proxy@users.noreply.github.com>
2026-09-21 21:37:34 +0300

    Fix #17 EnterpriseContext reassignment guard

--- app/Filament/Resources/EnterpriseContexts/Pages/EditEnterpriseContext.php
+++ app/Filament/Resources/EnterpriseContexts/Pages/EditEnterpriseContext.php
@@ -8,8 +8,9 @@ use Illuminate\Auth\Access\AuthorizationException;
 class EditEnterpriseContext extends EditRecord {
  protected static string $resource=EnterpriseContextResource::class;
  protected function mutateFormDataBeforeSave(array $data):array{
-  /** @var EnterpriseContext $record */ $record=$this->record;
-  if((int)$data['${parent}_id']!==$record->${parent}_id){throw new AuthorizationException('Cannot reassign this record.');}
+  /** @var EnterpriseContext $record */
+  $record=$this->record;
+  if((int)$data['enterprise_id']!==$record->enterprise_id){throw new AuthorizationException('Cannot reassign this record.');}
   return $data;
  }
  protected function getHeaderActions():array{return [DeleteAction::make()];}
