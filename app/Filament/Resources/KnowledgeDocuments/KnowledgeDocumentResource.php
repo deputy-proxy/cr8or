4879be3a508ad496d96324d9e5836256ee355fc7
Proxy for Main <149622634+deputy-proxy@users.noreply.github.com>
2026-09-22 22:52:16 +0300

    Implement knowledge foundation

---
+++ app/Filament/Resources/KnowledgeDocuments/KnowledgeDocumentResource.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Filament\Resources\KnowledgeDocuments;
+
+use App\Filament\Resources\Concerns\ScopesKnowledgeRecords;
+use App\Filament\Resources\KnowledgeDocuments\Pages\CreateKnowledgeDocument;
+use App\Filament\Resources\KnowledgeDocuments\Pages\EditKnowledgeDocument;
+use App\Filament\Resources\KnowledgeDocuments\Pages\ListKnowledgeDocuments;
+use App\Filament\Resources\KnowledgeDocuments\Schemas\KnowledgeDocumentForm;
+use App\Filament\Resources\KnowledgeDocuments\Tables\KnowledgeDocumentsTable;
+use App\Models\KnowledgeDocument;
+use BackedEnum;
+use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Table;
+
+class KnowledgeDocumentResource extends Resource
+{
+    use ScopesKnowledgeRecords;
+
+    protected static ?string $model = KnowledgeDocument::class;
+
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedBookOpen;
+
+    public static function form(Schema $schema): Schema
+    {
+        return KnowledgeDocumentForm::configure($schema);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return KnowledgeDocumentsTable::configure($table);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ListKnowledgeDocuments::route('/'),
+            'create' => CreateKnowledgeDocument::route('/create'),
+            'edit' => EditKnowledgeDocument::route('/{record}/edit'),
+        ];
+    }
+}
