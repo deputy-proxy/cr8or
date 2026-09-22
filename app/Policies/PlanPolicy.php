97ff77d977d59b8d6a2027f528eac92e55b486b5
Proxy for Main <149622634+deputy-proxy@users.noreply.github.com>
2026-09-22 23:37:06 +0300

    feat: implement strategy planning domain

---
+++ app/Policies/PlanPolicy.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Plan;
+use App\Models\Strategy;
+use App\Models\User;
+
+class PlanPolicy
+{
+    public function view(User $user, Plan $plan): bool
+    {
+        return (new EnterprisePolicy)->view($user, $plan->strategy->objective->enterprise);
+    }
+
+    public function create(User $user, Strategy $strategy): bool
+    {
+        return (new EnterprisePolicy)->create($user, $strategy->objective->enterprise->organization);
+    }
+
+    public function update(User $user, Plan $plan): bool
+    {
+        return (new EnterprisePolicy)->update($user, $plan->strategy->objective->enterprise);
+    }
+
+    public function delete(User $user, Plan $plan): bool
+    {
+        return (new EnterprisePolicy)->delete($user, $plan->strategy->objective->enterprise);
+    }
+}
