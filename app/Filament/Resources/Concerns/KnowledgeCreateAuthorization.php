0c2e5664a31d752d04873fc9e50e9558ffb222bf
Proxy for Main <149622634+deputy-proxy@users.noreply.github.com>
2026-09-22 22:51:26 +0300

    Implement knowledge foundation

---
+++ app/Filament/Resources/Concerns/KnowledgeCreateAuthorization.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Filament\Resources\Concerns;
+
+use App\Models\Enterprise;
+use Illuminate\Support\Facades\Gate;
+
+trait KnowledgeCreateAuthorization
+{
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $enterprise = Enterprise::query()->findOrFail((int) $data['enterprise_id']);
+        Gate::authorize('create', [$this->getResource()::getModel(), $enterprise]);
+
+        return $data;
+    }
+}
