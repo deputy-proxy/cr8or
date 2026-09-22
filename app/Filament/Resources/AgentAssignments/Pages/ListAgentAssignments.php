1e2672c15d08215d5ac5b5726f589029bfc2f066
Proxy for Main <149622634+deputy-proxy@users.noreply.github.com>
2026-09-22 20:46:19 +0300

    #33 add governance administration

---
+++ app/Filament/Resources/AgentAssignments/Pages/ListAgentAssignments.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\AgentAssignments\Pages;
+
+use App\Filament\Resources\AgentAssignments\AgentAssignmentResource;
+use Filament\Resources\Pages\ListRecords;
+
+class ListAgentAssignments extends ListRecords
+{
+    protected static string $resource = AgentAssignmentResource::class;
+}
