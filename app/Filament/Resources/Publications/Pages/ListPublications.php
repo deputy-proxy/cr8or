1bfa38e3713bfcada5b383ea1ca370d4b6ea0641
Proxy for Main <149622634+deputy-proxy@users.noreply.github.com>
2026-09-23 19:26:00 +0300

    feat: add phase 5 administration UI

---
+++ app/Filament/Resources/Publications/Pages/ListPublications.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\Publications\Pages;
+
+use App\Filament\Resources\Publications\PublicationResource;
+use Filament\Resources\Pages\ListRecords;
+
+class ListPublications extends ListRecords
+{
+    protected static string $resource = PublicationResource::class;
+}
