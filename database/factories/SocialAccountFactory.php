6cd30a296a102caa996946fe9ec2dc5ce8f1081b
Proxy for Main <149622634+deputy-proxy@users.noreply.github.com>
2026-09-23 18:38:40 +0300

    style: format social account factory

--- database/factories/SocialAccountFactory.php
+++ database/factories/SocialAccountFactory.php
@@ -7,13 +7,21 @@ use App\Models\Enterprise;
 use App\Models\SocialAccount;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/** @extends Factory<SocialAccount> */ class SocialAccountFactory extends Factory
+/** @extends Factory<SocialAccount> */
+class SocialAccountFactory extends Factory
 {
     public function definition(): array
     {
         $e = Enterprise::factory();
         $c = Channel::factory()->for($e);
 
-        return ['enterprise_id' => $e, 'channel_id' => $c, 'provider' => 'postiz', 'name' => fake()->company(), 'external_id' => fake()->unique()->bothify('postiz-####'), 'status' => SocialAccount::STATUS_ACTIVE];
+        return [
+            'enterprise_id' => $e,
+            'channel_id' => $c,
+            'provider' => 'postiz',
+            'name' => fake()->company(),
+            'external_id' => fake()->unique()->bothify('postiz-####'),
+            'status' => SocialAccount::STATUS_ACTIVE,
+        ];
     }
 }
