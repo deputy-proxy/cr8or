616839a3a7aa7de503b89dd4cc6951b5871e0ff6
Proxy for Main <149622634+deputy-proxy@users.noreply.github.com>
2026-09-23 23:53:33 +0300

    #[84] implemented

---
+++ app/Filament/Resources/Revenues/Pages/ListRevenues.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\Revenues\Pages;
+
+use App\Filament\Resources\Revenues\RevenueResource;
+use Filament\Resources\Pages\ListRecords;
+
+class ListRevenues extends ListRecords
+{
+    protected static string $resource = RevenueResource::class;
+}
