3dc80a4c8749186fa1caaad84222f3d4085bc768
Proxy for Main <149622634+deputy-proxy@users.noreply.github.com>
2026-09-23 23:50:31 +0300

    #[84] implemented

---
+++ app/Filament/Resources/Budgets/Pages/ListBudgets.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\Budgets\Pages;
+
+use App\Filament\Resources\Budgets\BudgetResource;
+use Filament\Resources\Pages\ListRecords;
+
+class ListBudgets extends ListRecords
+{
+    protected static string $resource = BudgetResource::class;
+}
