9d7b3f6dab27a9ff4a38541f54505a00f78f9a1c
Proxy for Main <149622634+deputy-proxy@users.noreply.github.com>
2026-09-23 19:25:21 +0300

    feat: add phase 5 administration UI

---
+++ app/Filament/Resources/PublicationSchedules/Pages/EditPublicationSchedule.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\PublicationSchedules\Pages;
+
+use App\Filament\Resources\PublicationSchedules\PublicationScheduleResource;
+use Filament\Resources\Pages\EditRecord;
+
+class EditPublicationSchedule extends EditRecord
+{
+    protected static string $resource = PublicationScheduleResource::class;
+}
