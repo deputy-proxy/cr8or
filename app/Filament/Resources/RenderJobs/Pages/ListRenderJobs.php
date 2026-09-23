8e25e832753c4a83bd2159b3fb4d7a0dfed6efd8
Proxy for Main <149622634+deputy-proxy@users.noreply.github.com>
2026-09-23 19:26:30 +0300

    feat: add phase 5 administration UI

---
+++ app/Filament/Resources/RenderJobs/Pages/ListRenderJobs.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\RenderJobs\Pages;
+
+use App\Filament\Resources\RenderJobs\RenderJobResource;
+use Filament\Resources\Pages\ListRecords;
+
+class ListRenderJobs extends ListRecords
+{
+    protected static string $resource = RenderJobResource::class;
+}
