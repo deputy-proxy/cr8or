a3ccd71e0c15d3dc8d05d0e1a3a0d94442229950
Proxy for Main <149622634+deputy-proxy@users.noreply.github.com>
2026-09-23 23:53:51 +0300

    #[84] implemented

---
+++ app/Filament/Resources/StatementEntries/Pages/EditStatementEntries.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\StatementEntries\Pages;
+
+use App\Filament\Resources\StatementEntries\StatementEntryResource;
+use Filament\Resources\Pages\EditRecord;
+
+class EditStatementEntries extends EditRecord
+{
+    protected static string $resource = StatementEntryResource::class;
+}
