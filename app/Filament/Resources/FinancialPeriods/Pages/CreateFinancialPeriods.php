2d5779ac43f12bd767feaeffadd1350af17896eb
Proxy for Main <149622634+deputy-proxy@users.noreply.github.com>
2026-09-23 23:52:05 +0300

    #[84] implemented

---
+++ app/Filament/Resources/FinancialPeriods/Pages/CreateFinancialPeriods.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\FinancialPeriods\Pages;
+
+use App\Filament\Resources\FinancialPeriods\FinancialPeriodResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateFinancialPeriods extends CreateRecord
+{
+    protected static string $resource = FinancialPeriodResource::class;
+}
