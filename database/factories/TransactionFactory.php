5f5c3619646860e2f6feb9b1867e8fe58b3825c7
Proxy for Main <149622634+deputy-proxy@users.noreply.github.com>
2026-09-23 21:12:01 +0300

    Implement issue #77: add TransactionFactory.php

---
+++ database/factories/TransactionFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Enterprise;
+use App\Models\FinancialAccount;
+use App\Models\Transaction;
+use App\Models\TransactionCategory;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/** @extends Factory<Transaction> */
+class TransactionFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'enterprise_id' => Enterprise::factory(),
+            'financial_account_id' => fn (array $attributes): int => FinancialAccount::factory()
+                ->create(['enterprise_id' => $attributes['enterprise_id']])
+                ->getKey(),
+            'transaction_category_id' => fn (array $attributes): int => TransactionCategory::factory()
+                ->create(['enterprise_id' => $attributes['enterprise_id']])
+                ->getKey(),
+            'amount' => fake()->randomFloat(4, -100000, 100000),
+            'transaction_date' => fake()->date(),
+            'description' => fake()->sentence(),
+            'reference' => fake()->optional()->bothify('TX-########'),
+        ];
+    }
+}
