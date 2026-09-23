04be813200dfe57ffa8ac388020a73f54cc3d1d5
Proxy for Main <149622634+deputy-proxy@users.noreply.github.com>
2026-09-23 21:11:50 +0300

    Implement issue #77: add TransactionPolicy.php

---
+++ app/Policies/TransactionPolicy.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Enterprise;
+use App\Models\Transaction;
+use App\Models\User;
+
+class TransactionPolicy
+{
+    public function view(User $user, Transaction $transaction): bool
+    {
+        return $this->enterprisePolicy()->view($user, $transaction->enterprise);
+    }
+
+    public function create(User $user, Enterprise $enterprise): bool
+    {
+        return $this->enterprisePolicy()->create($user, $enterprise->organization);
+    }
+
+    public function update(User $user, Transaction $transaction): bool
+    {
+        return $this->enterprisePolicy()->update($user, $transaction->enterprise);
+    }
+
+    public function delete(User $user, Transaction $transaction): bool
+    {
+        return $this->enterprisePolicy()->delete($user, $transaction->enterprise);
+    }
+
+    private function enterprisePolicy(): EnterprisePolicy
+    {
+        return new EnterprisePolicy;
+    }
+}
