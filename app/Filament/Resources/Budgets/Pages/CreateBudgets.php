c94294c64e995f6e7c2a06f16856d7cdbebbdf10
Proxy for Main <149622634+deputy-proxy@users.noreply.github.com>
2026-09-23 23:50:09 +0300

    #[84] implemented

---
+++ app/Filament/Resources/Budgets/Pages/CreateBudgets.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\Budgets\Pages;
+
+use App\Filament\Resources\Budgets\BudgetResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateBudgets extends CreateRecord
+{
+    protected static string $resource = BudgetResource::class;
+}
