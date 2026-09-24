5b862f71cdeb84816d2698046023e0341e34f9e0
Proxy for Main <149622634+deputy-proxy@users.noreply.github.com>
2026-09-24 11:03:34 +0300

    Add phase seven agent administration

---
+++ app/Filament/Resources/AgentDelegations/Pages/ListAgentDelegations.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\AgentDelegations\Pages;
+
+use App\Filament\Resources\AgentDelegations\AgentDelegationResource;
+use Filament\Resources\Pages\ListRecords;
+
+class ListAgentDelegations extends ListRecords
+{
+    protected static string $resource = AgentDelegationResource::class;
+}
