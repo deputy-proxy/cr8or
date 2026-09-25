fc7761f36de206340d53fcd861dd4af09a6a92e7
Proxy for Main <149622634+deputy-proxy@users.noreply.github.com>
2026-09-25 10:32:35 +0300

    Fix Filament create authorization

--- app/Filament/Resources/ContentSeries/Pages/CreateContentSeries.php
+++ app/Filament/Resources/ContentSeries/Pages/CreateContentSeries.php
@@ -3,9 +3,20 @@
 namespace App\Filament\Resources\ContentSeries\Pages;
 
 use App\Filament\Resources\ContentSeries\ContentSeriesResource;
+use App\Models\Campaign;
+use App\Models\ContentSeries;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Facades\Gate;
 
 class CreateContentSeries extends CreateRecord
 {
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $campaign = Campaign::query()->findOrFail((int) $data['campaign_id']);
+        Gate::authorize('createForCampaign', [ContentSeries::class, $campaign]);
+
+        return $data;
+    }
+
     protected static string $resource = ContentSeriesResource::class;
 }
--- app/Filament/Resources/MarketingStrategies/Pages/CreateMarketingStrategy.php
+++ app/Filament/Resources/MarketingStrategies/Pages/CreateMarketingStrategy.php
@@ -2,10 +2,13 @@
 
 namespace App\Filament\Resources\MarketingStrategies\Pages;
 
+use App\Filament\Resources\Concerns\EnterpriseCreateAuthorization;
 use App\Filament\Resources\MarketingStrategies\MarketingStrategyResource;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateMarketingStrategy extends CreateRecord
 {
+    use EnterpriseCreateAuthorization;
+
     protected static string $resource = MarketingStrategyResource::class;
 }
