1089799a6bcce6833bbdb0776a84be018af6dc61
Proxy for Main <149622634+deputy-proxy@users.noreply.github.com>
2026-09-25 07:04:31 +0300

    Implement domain-based Filament navigation

--- app/Filament/Resources/RenderJobs/RenderJobResource.php
+++ app/Filament/Resources/RenderJobs/RenderJobResource.php
@@ -21,6 +21,12 @@ class RenderJobResource extends Resource
 
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
 
+    protected static string|\UnitEnum|null $navigationGroup = 'Media';
+
+    protected static ?string $navigationLabel = 'Render Jobs';
+
+    protected static ?int $navigationSort = 70;
+
     public static function form(Schema $schema): Schema
     {
         return $schema->components([
