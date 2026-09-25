30c6f84017ff10bedc91198421fe14c64aa411da
Proxy for Main <149622634+deputy-proxy@users.noreply.github.com>
2026-09-25 07:01:43 +0300

    Implement domain-based Filament navigation

--- app/Filament/Resources/IntegrationConnections/IntegrationConnectionResource.php
+++ app/Filament/Resources/IntegrationConnections/IntegrationConnectionResource.php
@@ -27,6 +27,12 @@ class IntegrationConnectionResource extends Resource
 
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
 
+    protected static string|\UnitEnum|null $navigationGroup = 'Integrations';
+
+    protected static ?string $navigationLabel = 'Connections';
+
+    protected static ?int $navigationSort = 10;
+
     public static function form(Schema $schema): Schema
     {
         return $schema->components([
