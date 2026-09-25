c4dc8ff18e61cd3b92a683b7a0a67a77efbbb9eb
Proxy for Main <149622634+deputy-proxy@users.noreply.github.com>
2026-09-25 07:04:49 +0300

    Implement domain-based Filament navigation

--- app/Filament/Resources/Revenues/RevenueResource.php
+++ app/Filament/Resources/Revenues/RevenueResource.php
@@ -26,6 +26,12 @@ class RevenueResource extends Resource
 
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedArrowTrendingUp;
 
+    protected static string|\UnitEnum|null $navigationGroup = 'Operations';
+
+    protected static ?string $navigationLabel = 'Revenue';
+
+    protected static ?int $navigationSort = 180;
+
     public static function form(Schema $schema): Schema
     {
         return $schema->components([
