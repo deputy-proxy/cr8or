a4d04b683a0c6d2411f0fbb559b422ae1ca84be6
Proxy for Main <149622634+deputy-proxy@users.noreply.github.com>
2026-09-25 07:02:07 +0300

    Implement domain-based Filament navigation

--- app/Filament/Resources/Invoices/InvoiceResource.php
+++ app/Filament/Resources/Invoices/InvoiceResource.php
@@ -26,6 +26,12 @@ class InvoiceResource extends Resource
 
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedReceiptPercent;
 
+    protected static string|\UnitEnum|null $navigationGroup = 'Operations';
+
+    protected static ?string $navigationLabel = 'Invoices';
+
+    protected static ?int $navigationSort = 170;
+
     public static function form(Schema $schema): Schema
     {
         return $schema->components([
